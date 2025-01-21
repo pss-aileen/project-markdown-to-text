e8f34abf905659731a5f8274c6222ea0851c9800
Aileen <140990100+pss-aileen@users.noreply.github.com>
2025-01-21 18:34:40 +0900

    done logic

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,5 @@
 import './style.css';
 
-/* 
-  - - を識別する
-  - 余白を識別する
-  - 階層1つ目 []
-  - 階層2つ目　/
-  - 階層3つ目 ,
-  - 行で区切る
-  - そこから形成する
-*/
-
 const inputElement = document.getElementById('input') as HTMLTextAreaElement;
 const outputElement = document.getElementById('output') as HTMLTextAreaElement;
 const convertBtn = document.getElementById('btn-convert') as HTMLButtonElement;
@@ -24,59 +14,72 @@ convertBtn.addEventListener('click', () => {
     const spaceLength = getSpaceLength(string);
     const trimedText = string.trimStart();
 
-    // もし `-` が先頭にこばない場合はスルーする
-
     if (!trimedText.startsWith('-')) {
-      return;
+      return string;
     }
 
-    const removeHyphenText = trimedText.replace('- ', '');
-    console.log(removeHyphenText);
+    const removeHyphenText = trimedText.replace('- ', '').trim();
+
+    const removedEndColonText = removeHyphenText.endsWith(':') ? removeHyphenText.slice(0, -1) : removeHyphenText;
 
     let symbolStart: string = '';
     let symbolEnd: string = '';
+
+    console.log(removedEndColonText, spaceLength);
+
     if (spaceLength === 0) {
-      symbolStart = '【 ';
-      symbolEnd = ' 】';
-    } else if (spaceLength === 2) {
-      symbolStart = '';
-      symbolEnd = ', ';
-    } else if (spaceLength === 4) {
-      symbolStart = '';
-      symbolEnd = ', ';
-    }
+      symbolStart = '[ ';
+      symbolEnd = ' ]';
 
-    // 前のスペースと比べて、大きくなっていれば、 / で区切りを入れる
-    if (spaceLength !== 0 && array[index - 1]) {
-      const prevSpaceLength = getSpaceLength(array[index - 1]);
-      console.log(prevSpaceLength, spaceLength);
-
-      if (spaceLength <= 2 && prevSpaceLength > spaceLength) {
-        symbolStart = ' / ';
-      }
-      if (prevSpaceLength === spaceLength) {
-        symbolEnd = ', ';
+      if (array[index - 1]) {
+        symbolStart = '\n[ ';
       }
     }
 
-    if (array[index + 1]) {
-      const nextSpaceLength = getSpaceLength(array[index + 1]);
+    if (spaceLength === 2) {
+      if (array[index - 1]) {
+        const prevSpaceLength = getSpaceLength(array[index - 1]);
+        console.log(prevSpaceLength, spaceLength);
 
-      if (spaceLength !== 0 && spaceLength > nextSpaceLength) {
-        symbolEnd = '';
+        symbolStart = ' ';
+
+        if (prevSpaceLength > spaceLength || prevSpaceLength === spaceLength) {
+          symbolStart = ' / ';
+        }
       }
-    } else {
-      symbolEnd = '';
+
+      if (array[index + 1]) {
+        const nextSpaceLength = getSpaceLength(array[index + 1]);
+        if (spaceLength !== nextSpaceLength && nextSpaceLength > spaceLength) {
+          symbolEnd = ' - ';
+        }
+      }
     }
 
-    return symbolStart + removeHyphenText + symbolEnd;
+    if (spaceLength === 4) {
+      symbolStart = ', ';
+
+      if (array[index - 1]) {
+        const prevSpaceLength = getSpaceLength(array[index - 1]);
+
+        if (prevSpaceLength < spaceLength) {
+          symbolStart = '';
+        }
+      }
+    }
+
+    if (spaceLength >= 6) {
+      console.log('6以上', spaceLength);
+
+      symbolStart = ', ';
+    }
+
+    return symbolStart + removedEndColonText.trim() + symbolEnd;
   });
 
   function getSpaceLength(string: string) {
     return string.length - string.trimStart().length;
   }
-
-  console.log(editedLines);
 
   outputElement.value = editedLines.join('');
 });
